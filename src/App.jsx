8e40ac965ebe32d765dd757ade6eafd6ae6352cd
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2024-09-28 14:27:29 -0500

    Creación del componente Hero.jsx que sirve como inicio de la plataforma

--- src/App.jsx
+++ src/App.jsx
@@ -13,21 +13,19 @@ import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div className='bg-slate-100'>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] bg-slate-100'>
       <Navbar/>
-      <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-          <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/collection' element={<Collection/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/contact' element={<Contact/>}/>
-          <Route path='/product/:productId' element={<Product/>}/>
-          <Route path='/cart' element={<Cart/>}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/place-order' element={<PlaceOrder/>}/>
-          <Route path='/orders' element={<Orders/>}/>
-        </Routes>
-      </div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/product/:productId' element={<Product/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/place-order' element={<PlaceOrder/>}/>
+        <Route path='/orders' element={<Orders/>}/>
+      </Routes>
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     const [visible, setVisible] = useState(false);
 
     return (
-        <div className='bg-slate-300 flex items-center justify-between py-5 px-20 font-medium w-full'>
+        <div className='flex items-center justify-between py-5 font-medium'>
             <img src={assets.logo} className='w-36' alt=''/>
             <ul className='hidden sm:flex gap-5 text-sm text-gray-700'>
                 <NavLink to='/' className='flex flex-col items-center gap-1'>
