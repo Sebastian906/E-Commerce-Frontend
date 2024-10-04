a2d9fd2498387069ffc0a4dddb0d2434468f7268
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2024-10-04 15:20:11 -0500

    Configuracion en la interfaz gráfica de la página de ordenes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,11 +37,11 @@ const Navbar = () => {
                     <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-slate-900 text-white aspect-square rounded-full text-[8px]'>{getCartCount()}</p>
                 </Link>
                 <div className='group relative'>
-                    <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
+                    <Link to='/login'><img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" /></Link>
                     <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
                         <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
                             <p className='cursor-pointer hover:text-black'>Mi Perfil</p>
-                            <p className='cursor-pointer hover:text-black'>Ordenes</p>
+                            <Link to='/orders'><p className='cursor-pointer hover:text-black'>Ordenes</p></Link>
                             <p className='cursor-pointer hover:text-black'>Cerrar Sesión</p>
                         </div>
                     </div>
