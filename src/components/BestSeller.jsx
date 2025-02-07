a18335d4fa89863e87da800d02143b0eb54f4ab8
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2025-02-07 15:09:39 -0500

    Cambio en el componente de bestseller para actualizar la muestra de productos mas vendidos

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -3,14 +3,14 @@ import { ShopContext } from '../context/ShopContext'
 import Title from './Title';
 import ProductItem from './ProductItem';
 
-const BestSeller = () => {
+const bestseller = () => {
 
     const {products} = useContext(ShopContext);
-    const [bestSeller,setBestSeller] = useState([]);
+    const [bestseller,setbestseller] = useState([]);
 
     useEffect(()=>{
         const bestProduct = products.filter((item)=>(item.bestseller));
-        setBestSeller(bestProduct.slice(0,5))
+        setbestseller(bestProduct.slice(0,5))
     },[products])
 
     return (
@@ -23,7 +23,7 @@ const BestSeller = () => {
             </div>
             <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6'>
                 {
-                    bestSeller.map((item,index)=>(
+                    bestseller.map((item,index)=>(
                         <ProductItem key={index} id={item._id} name={item.name} image={item.image} price={item.price}/>
                     ))
                 }
@@ -32,4 +32,4 @@ const BestSeller = () => {
     )
 }
 
-export default BestSeller
+export default bestseller
