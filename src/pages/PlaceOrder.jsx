9d3b0cc3f3e4fb3848b16323eb6d84192dddff67
unknown <sebastian.1701811396@ucaldas.edu.co>
2025-02-16 14:27:30 -0500

    Se agrega la función lógica para el formulario de ordenes en el frontend

--- src/pages/PlaceOrder.jsx
+++ src/pages/PlaceOrder.jsx
@@ -3,35 +3,95 @@ import Title from '../components/Title'
 import CartTotal from '../components/CartTotal'
 import { assets } from '../assets/assets'
 import { ShopContext } from '../context/ShopContext'
+import axios from 'axios'
+import { toast } from 'react-toastify'
 
 const PlaceOrder = () => {
 
     const [method,setMethod] = useState('cod');
+    const {navigate, backendUrl, token, cartItems, setCartItems, getCartAmount, delivery_fee, products} = useContext(ShopContext);
+    const [formData, setFormData] = useState({
+        firstName:'',
+        lastName:'',
+        email:'',
+        street:'',
+        city:'',
+        state:'',
+        zipcode:'',
+        country:'',
+        phone:''
+    })
 
-    const {navigate} = useContext(ShopContext);
+    const onChangeHandler = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setFormData(data => ({...data,[name]:value}))
+    }
+
+    const onSubmitHandler = async () => {
+        event.preventDefault()
+        try {
+            let orderItems = []
+            for(const items in cartItems) {
+                for(const item in cartItems[items]) {
+                    if (cartItems[items][item] > 0) {
+                        const itemInfo = structuredClone(products.find(product => product._id === items))
+                        if (itemInfo) {
+                            itemInfo.size = item
+                            itemInfo.quantity = cartItems[items][item]
+                            orderItems.push(itemInfo)
+                        }
+                    }
+                }
+            }
+            let orderData = {
+                address: formData,
+                items: orderItems,
+                amount: getCartAmount() + delivery_fee
+            }
+
+            switch(method) {
+                // API Calls for COD
+                case 'cod':
+                    const response = await axios.post(backendUrl + '/api/order/place',orderData,{headers:{token}});
+                    if (response.data.success) {
+                        setCartItems({})
+                        navigate('/orders')
+                    } else {
+                        toast.error(response.data.message)
+                    }
+                    break;
+                default:
+                    break;
+            }
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        }
+    }
 
     return (
-        <div className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t'>
+        <form onSubmit={onSubmitHandler} className='flex flex-col sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t'>
             {/** Lado Izquierdo */}
             <div className='flex flex-col gap-4 w-full sm:max-w-[480px]'>
                 <div className='text-xl sm:text-2xl my-3'>
                     <Title text1={'INFORMACIÓN'} text2={'DE ENVÍO'}/>
                 </div>
                 <div className='flex gap-3'>
-                    <input className='border bg-slate-100 border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Primer Nombre' />
-                    <input className='border bg-slate-100 border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Apellido' />
-                </div>
-                <input className='border bg-slate-100 border-gray-300 rounded py-1.5 px-3.5 w-full' type="email" placeholder='Correo Electrónico' />
-                <input className='border bg-slate-100 border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Dirección' />
+                    <input required onChange={onChangeHandler} name='firstName' value={formData.firstName} className='border bg-slate-100 border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Primer Nombre' />
+                    <input required onChange={onChangeHandler} name='lastName' value={formData.lastName} className='border bg-slate-100 border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Apellido' />
+                </div>                
+                <input required onChange={onChangeHandler} name='email' value={formData.email} className='border bg-slate-100 border-gray-300 rounded py-1.5 px-3.5 w-full' type="email" placeholder='Correo Electrónico' />
+                <input required onChange={onChangeHandler} name='street' value={formData.street} className='border bg-slate-100 border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Dirección' />
                 <div className='flex gap-3'>
-                    <input className='border bg-slate-100 border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Ciudad' />
-                    <input className='border bg-slate-100 border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Departamento' />
+                    <input required onChange={onChangeHandler} name='city' value={formData.city} className='border bg-slate-100 border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Ciudad' />
+                    <input required onChange={onChangeHandler} name='state' value={formData.state} className='border bg-slate-100 border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Departamento' />
                 </div>
                 <div className='flex gap-3'>
-                    <input className='border bg-slate-100 border-gray-300 rounded py-1.5 px-3.5 w-full' type="number" placeholder='Código ZIP' />
-                    <input className='border bg-slate-100 border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='País' />
+                    <input required onChange={onChangeHandler} name='zipcode' value={formData.zipcode} className='border bg-slate-100 border-gray-300 rounded py-1.5 px-3.5 w-full' type="number" placeholder='Código ZIP' />
+                    <input required onChange={onChangeHandler} name='country' value={formData.country} className='border bg-slate-100 border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='País' />
                 </div>
-                <input className='border bg-slate-100 border-gray-300 rounded py-1.5 px-3.5 w-full' type="number" placeholder='Teléfono' />
+                <input required onChange={onChangeHandler} name='phone' value={formData.phone} className='border bg-slate-100 border-gray-300 rounded py-1.5 px-3.5 w-full' type="number" placeholder='Teléfono' />
             </div>
             {/** Lado Derecho */}
             <div className='mt-8'>
@@ -56,11 +116,11 @@ const PlaceOrder = () => {
                         </div>
                     </div>
                     <div className='w-full text-end mt-8'>
-                        <button onClick={()=>navigate('/orders')} className='bg-black text-gray-100 px-20 py-3 text-sm hover:bg-gray-700 transition-all active:bg-gray-700'>REALIZAR ORDEN</button>
+                        <button type='submit' className='bg-black text-gray-100 px-20 py-3 text-sm hover:bg-gray-700 transition-all active:bg-gray-700'>REALIZAR ORDEN</button>
                     </div>
                 </div>
             </div>
-        </div>
+        </form>
     )
 }
 
